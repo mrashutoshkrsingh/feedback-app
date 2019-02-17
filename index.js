2f557c44f74641e911aac8b6a4077f63dd5f07e2
Ashutosh Kumar <mrashutoshkrsingh@gmail.com>
2019-02-17 03:57:02 +0000

    adding auth

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express=require("express");
+const passport = require('passport');
 const app=express();
+const mongoose=require("mongoose");
+const cookieSession = require('cookie-session');
+const keys=require("./config/keys")
 
-app.get("/",(req,res)=>{
-    res.send({text:'hi'});
-})
+require("./services/passport");
+require('./models/User');
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+mongoose.connect(keys.mongoURI,{useNewUrlParser:true})
+.then(console.log("database connected"));
+
+require('./routes/authRoutes')(app);
+
+
+
 
 app.listen(process.env.PORT,process.env.IP)
